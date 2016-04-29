b68be3eb03135a5bd0aab0ae716d27a57b42a254
Christoph Petersen <cp@peterschen.de>
2016-04-29 11:20:11 +0200

    Removed duplicate load of jquery Moved js to site.js

--- SudoGenerator/wwwroot/js/site.js
+++ SudoGenerator/wwwroot/js/site.js
@@ -2,3 +2,32 @@ $(document).ready(function () {
     $('.input-validation-error').parents('.form-group').addClass('has-error');
     $('.field-validation-error').addClass('text-danger');
 });
+
+function loadData(result) {
+    var payload = {
+        type: "os",
+        ProductVersion: $("#ProductVersion").val()
+    };
+
+    $.getJSON('/Home/Data', payload, function (result) {
+        var dropdown = $('#OperatingSystem');
+        dropdown.empty();
+
+        $(result).each(function () {
+            $(document.createElement('option'))
+                .attr('value', this.Id)
+                .text(this.Value)
+                .appendTo(dropdown);
+        });
+
+        dropdown.prop("disabled", false);
+    });
+}
+
+$("#ProductVersion").change(function () {
+    loadData();
+});
+
+$(function () {
+    loadData()
+});
