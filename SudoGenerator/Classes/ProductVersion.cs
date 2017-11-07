4160b64caa25c5a374525a21cd69894b66c00cff
Christoph Petersen <cp@peterschen.de>
2017-11-07 12:10:18 +0100

    Removed beta flag for OM16

--- SudoGenerator/Classes/ProductVersion.cs
+++ SudoGenerator/Classes/ProductVersion.cs
@@ -9,7 +9,7 @@ namespace SudoGenerator.Classes
 
         public static readonly ProductVersion OM2012RTM = new ProductVersion("2012-rtm", "2012 RTM");
         public static readonly ProductVersion OM2012R2 = new ProductVersion("2012-r2", "2012 SP1 / 2012 R2");
-        public static readonly ProductVersion OM2016RTM = new ProductVersion("2016-rtm", "2016 RTM (Beta)");
+        public static readonly ProductVersion OM2016RTM = new ProductVersion("2016-rtm", "2016 RTM");
 
         public static readonly List<ProductVersion> ALL = new List<ProductVersion> { OM2012RTM, OM2012R2, OM2016RTM };
 
